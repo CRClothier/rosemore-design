ab1276d68364a80cd3d8a410982a3528ecef5c4b
Chris Clothier <c.r.clothier@gmail.com>
2024-02-15 17:24:56 +0000

    Add slide rotation and initial fade in animation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-const autoRotate = false;
+const autoRotate = true;
 const intervalTime = 5000;
 let slideInterval;
 
  const nextSlide = () => {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, intervalTime);
   const visible = document.querySelector('.visible');
   visible.classList.remove('visible');
   if (visible.nextElementSibling) {
@@ -16,6 +18,8 @@ let slideInterval;
  }
 
  const prevSlide = () => {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, intervalTime);
   const visible = document.querySelector('.visible');
   visible.classList.remove('visible');
   if (visible.previousElementSibling) {
@@ -28,3 +32,8 @@ let slideInterval;
 next.addEventListener('click', nextSlide);
 
 prev.addEventListener('click', prevSlide);
+
+if (autoRotate) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
