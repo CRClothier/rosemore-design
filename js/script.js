bd90ff61bcf22e67d7c66fcf01ebe42bc91bbe8d
Chris Clothier <c.r.clothier@gmail.com>
2024-02-15 18:58:25 +0000

    Add header and navbar with links to sections

--- js/script.js
+++ js/script.js
@@ -37,3 +37,15 @@ if (autoRotate) {
   // Run next slide at interval time
   slideInterval = setInterval(nextSlide, intervalTime);
 }
+
+const header = document.querySelector('#header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 600) {
+    header.classList.add('header-scroll');
+  } else {
+    header.classList.remove('header-scroll');
+  }
+})
+
+console.log(window.screenY)
